4858ca7e40489d6bc2d86b504378774bc7b889e9
StasRomanov <stasromanov801@gmail.com>
2020-05-30 19:16:04 +0300

    stats-2

--- js/start.js
+++ js/start.js
@@ -1,12 +1,13 @@
 'use strict';
 var GAP = 90;
 var MAX_HEIGHT_HISTOGRAM = 150;
+var HISTOGRAM_WIDTH = 40;
+var HISTOGRAM_X = 120;
+var HISTOGRAM_Y = 250;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_HEIGHT = 270;
 var CLOUD_WIDTH = 420;
-var HISTOGRAM_X = 120;
-var HISTOGRAM_Y = 250;
 
 var rectRender = function (ctx, x, y, w, h, color) {
   ctx.fillStyle = color;
@@ -28,9 +29,9 @@ window.renderStatistics = function (ctx, name, time) {
   writer(ctx, HISTOGRAM_X, 60, '16px PT Mono', 'handing', '#000000', 'Список результатов:');
   for (var i = 0; i < name.length; i++) {
     if (name[i] === 'Вы') {
-      rectRender(ctx, HISTOGRAM_X + (GAP * i), HISTOGRAM_Y, 40, -(time[i] / ratio), 'rgba(255, 0, 0, 1)');
+      rectRender(ctx, HISTOGRAM_X + (GAP * i), HISTOGRAM_Y, HISTOGRAM_WIDTH, -(time[i] / ratio), 'rgba(255, 0, 0, 1)');
     } else {
-      rectRender(ctx, HISTOGRAM_X + (GAP * i), HISTOGRAM_Y, 40, -(time[i] / ratio), 'hsl(242,' + Math.random() * Math.floor(100) + '%' + ' , 47%)');
+      rectRender(ctx, HISTOGRAM_X + (GAP * i), HISTOGRAM_Y, HISTOGRAM_WIDTH, -(time[i] / ratio), 'hsl(242,' + Math.random() * Math.floor(100) + '%' + ' , 47%)');
     }
     writer(ctx, HISTOGRAM_X + (GAP * i), CLOUD_HEIGHT, '16px PT Mono', 'bottom', '#000000', name[i]);
     writer(ctx, HISTOGRAM_X + (GAP * i), HISTOGRAM_Y - (time[i] / ratio), '16px PT Mono', 'bottom', '#000000', Math.round(time[i]));
