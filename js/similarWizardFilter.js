5cc532eacbf5a840cf4d13ea3eb05bb4e6884c83
StasRomanov <stasromanov801@gmail.com>
2020-07-02 10:48:19 +0300

    filter update

--- js/similarWizardFilter.js
+++ js/similarWizardFilter.js
@@ -1,6 +1,26 @@
 'use strict';
 
 (function () {
+  var coatEyesFilter = function (colorCoat, colorEyes) {
+    return window.utilData.allWizards.filter(function (wizard) {
+      return wizard.colorCoat === colorCoat && wizard.colorEyes === colorEyes;
+    });
+  };
+  var coatFilter = function (colorCoat, colorEyes) {
+    return window.utilData.allWizards.filter(function (wizard) {
+      return wizard.colorCoat === colorCoat && wizard.colorEyes !== colorEyes;
+    });
+  };
+  var eyesFilter = function (colorCoat, colorEyes) {
+    return window.utilData.allWizards.filter(function (wizard) {
+      return wizard.colorCoat !== colorCoat && wizard.colorEyes === colorEyes;
+    });
+  };
+  var anyWizardFilter = function (colorCoat, colorEyes) {
+    return window.utilData.allWizards.filter(function (wizard) {
+      return wizard.colorCoat !== colorCoat && wizard.colorEyes !== colorEyes;
+    });
+  };
 
   window.filter = function () {
     var coatCurrentFill = window.utilData.mainWizardCoat.style.fill;
@@ -11,26 +31,6 @@
     if (eyeCurrentFill === '') {
       eyeCurrentFill = 'black';
     }
-    var coatEyesFilter = function (colorCoat, colorEyes) {
-      return window.utilData.allWizards.filter(function (wizard) {
-        return wizard.colorCoat === colorCoat && wizard.colorEyes === colorEyes;
-      });
-    };
-    var coatFilter = function (colorCoat, colorEyes) {
-      return window.utilData.allWizards.filter(function (wizard) {
-        return wizard.colorCoat === colorCoat && wizard.colorEyes !== colorEyes;
-      });
-    };
-    var eyesFilter = function (colorCoat, colorEyes) {
-      return window.utilData.allWizards.filter(function (wizard) {
-        return wizard.colorCoat !== colorCoat && wizard.colorEyes === colorEyes;
-      });
-    };
-    var anyWizardFilter = function (colorCoat, colorEyes) {
-      return window.utilData.allWizards.filter(function (wizard) {
-        return wizard.colorCoat !== colorCoat && wizard.colorEyes !== colorEyes;
-      });
-    };
     var result = coatEyesFilter(coatCurrentFill, eyeCurrentFill);
     result = result.concat(coatFilter(coatCurrentFill, eyeCurrentFill));
     result = result.concat(eyesFilter(coatCurrentFill, eyeCurrentFill));
